0f9251365e36517bd50303cb4d775932f9c74930
Julien-FRA <julien.francois@hetic.net>
2024-07-25 17:59:21 +0200

    <feat/server>: Ajout du CRUD user

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+const UserRoute = require("./routes/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(process.env.DBURL)
+  .then(() => {
+    console.log("Database Connected Successfully!!");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database", err);
+    process.exit();
+  });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello blog application!");
 });
+
+app.use("/user", UserRoute);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
---
+++ server/src/interfaces/user.ts
@@ -0,0 +1,6 @@
+export default interface UserSchema {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+}
