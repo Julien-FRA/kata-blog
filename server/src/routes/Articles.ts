1352d67dcaa60430b47857b40a62562227e7695e
Julien-FRA <julien.francois@hetic.net>
2024-08-27 17:58:17 +0200

    <feat/server>: Recuperation des article d'un utilisateur

--- server/src/routes/Articles.ts
+++ server/src/routes/Articles.ts
@@ -5,7 +5,8 @@ const Auth = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/", ArticleController.findAll);
-router.get("/:id", ArticleController.findOne);
+// router.get("/:id", ArticleController.findOne);
+router.get("/user", Auth.authenticate, ArticleController.findUserArticle);
 router.post("/", Auth.authenticate, ArticleController.create);
 router.patch("/:id", Auth.authenticate, ArticleController.update);
 router.delete("/:id", Auth.authenticate, ArticleController.destroy);
